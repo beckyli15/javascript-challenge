2df9990fb845dd35dc1dd5f76fc618cbc572cb7a
beckyli15 <50608335+beckyli15@users.noreply.github.com>
2019-09-08 09:31:02 -0700

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,33 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var button = d3.select("#filter-btn");
+//var inputField = d3.select("#datetime");
+var tbody = d3.select("tbody");
+var resetbtn = d3.select("#reset-btn");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+
+//console.log the weather data from data.js
+var populate=(aliendata)=>{
+
+
+    data.forEach(ufo_sightings =>{
+     var row=tbody.append("tr");
+     columns.forEach(column => row.append("td").text(ufo_sightings[column])
+     )
+    });
+}
+populate(data);
+
+
+
+button.on("click",function(){
+    var inputElement = d3.select("#datetime");
+    var inputValue= inputElement.property("value");
+    console.log(inputValue);
+    console.log(tableData);
+    var filteredData=tableData.filter(sighting => sighting.datetime === inputValue);
+    console.log(filteredData);
+});
+
